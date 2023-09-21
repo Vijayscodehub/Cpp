fe8bcf40ea79c35ec1f45ec731e5553a2754be30
Vijay Mistry <115798320+Vijayscodehub@users.noreply.github.com>
2023-09-21 21:03:34 +0530

    Add files via upload

---
+++ tut10.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /* ********** Loops *************** 
+    1- For Loop
+    2- While loop
+    3- do- while loop
+    */
+
+
+    /*For Loop*/
+
+// syntax -> for loop
+
+    /*for (initializaiton; condition;updation){
+        
+        cout<<output;
+    } */
+
+
+    // for (int a = 1; a<=20;a++){
+        
+    //     cout<<a<<endl;
+    // } 
+
+    //infinie for loop
+
+    // for(int i = 1; 3<4;i++) {
+    //     cout<<i<<endl;
+    // }
+
+// syntax -> while loop
+
+    /*while(condition):
+    {
+        
+        c++ statements;
+    } */
+
+// printing 1-20 using while loop
+// int i = 1;
+//     while (i<=40){
+//         cout<<i<<endl;
+//         i++;
+//     } 
+
+
+    //infinie while loop
+// int i = 1;
+//     while(true) {
+//         cout<<i<<endl;
+//         i++;
+//     }
+
+// syntax -> do-while loop
+// int i = 1;
+//     do{
+//         cout<<i<<endl;
+//         i++;
+//     }while(i<=20);
+
+int i = 1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(false);
+    // it will run the program at least once
+
+
+    return 0;
+}
